4a4c6c00b4f6e135e24b40cd63edeac161c33a9f
kidthales <kidthales@agogpixel.com>
2022-08-11 12:23:21 -0700

    Clean up.

--- plugins/global-variable/global-variable-plugin.js
+++ plugins/global-variable/global-variable-plugin.js
@@ -5,27 +5,87 @@
  */
 (function () {
   /**
+   * Localization manager. Responsible for mapping our localization keys to
+   * their localized values.
+   *
    * @const
+   * @private
    */
   var locaManager = (function () {
+      /**
+       * Localization data. Maps locale key to localized look up object. Track
+       * all of your plugin localizations here.
+       *
+       * @const
+       * @private
+       */
       var locaData = {
+          /**
+           * Default english localizations.
+           */
           en: {
+            /**
+             * Required.
+             */
             PLUGIN_NAME: 'Global Variable Plugin',
+
+            /**
+             * Required.
+             */
             PLUGIN_DESCRIPTION: 'Provides a global variable for your scripts.',
+
+            /**
+             * Required.
+             */
             PLUGIN_AUTHOR: 'kidthales <kidthales@agogpixel.com>',
+
+            /**
+             * Required.
+             */
             PLUGIN_HELP: 'Variable name must be a valid JavaScript identifier.',
+
             PARAMETER_NAME_GLOBAL_VARIABLE_NAME: 'Global Variable Name:',
             PARAMETER_DEFAULT_VALUE_GLOBAL_VARIABLE_NAME: 'MyGlobal',
             PARAMETER_NAME_GLOBAL_VARIABLE_VALUE: 'Global Variable Value:'
           }
         },
+        /**
+         * Default locale. Used as a fallback if a given loca key is not
+         * found for currently set locale.
+         *
+         * @const
+         * @private
+         */
         locaDefault = 'en',
+        /**
+         * Special regex for matching loca keys we can inline in our plugin
+         * parameters, etc.
+         *
+         * @const
+         * @private
+         * @example 'loca(MY_LOCA_KEY)'
+         */
         inlineRegex = /^loca\((.+)\)$/,
+        /**
+         * Current locale.
+         *
+         * @private
+         */
         locaCurrent = locaDefault,
+        /**
+         * Localization manager API.
+         *
+         * @public
+         */
         self = {
           /**
-           * @param {string} key
-           * @returns {string}
+           * Get localized string for specified key. Look up is performed in this order:
+           *   1. Current locale (ie. 'en_CA').
+           *   2. Current locale prefix (ie. 'en').
+           *   3. Default locale.
+           *
+           * @param {string} key Loca key.
+           * @returns {string} Localized string when successful, loca key otherwise.
            */
           get: function (key) {
             var locaCurrentShort = locaCurrent.substring(0, 2);
@@ -37,80 +97,126 @@
               ? locaData[locaDefault][key]
               : key;
           },
+
           /**
-           * @returns {string}
+           * Get current locale.
+           *
+           * @returns {string} Current locale.
            */
           getLocale: function () {
             return locaCurrent;
           },
+
           /**
+           * Set current locale.
+           *
            * @param {string} locale
            */
           setLocale: function (locale) {
             locaCurrent = locale;
           },
+
           /**
-           * @param {import("pgmmv/agtk/plugins/plugin/parameter").AgtkParameter[]} parameters
-           * @returns {import("pgmmv/agtk/plugins/plugin/parameter").AgtkParameter[]}
+           * Localize plugin parameters in place.
+           *
+           * @param {import("pgmmv/agtk/plugins/plugin/parameter").AgtkParameter[]} parameters Non-localized plugin parameters.
+           * @returns {import("pgmmv/agtk/plugins/plugin/parameter").AgtkParameter[]} Localized plugin parameters.
            */
-          processParameterLocale: function (parameters) {
+          processParameters: function (parameters) {
             var pLen = parameters.length,
               i = 0,
               j = 0,
+              /** @type {import("pgmmv/agtk/plugins/plugin/parameter").AgtkParameter} */
               p,
+              /** @type {RegExpMatchArray | null} */
               m,
+              /** @type {number} */
               cpLen,
+              /** @type {import("pgmmv/agtk/plugins/plugin/parameter").AgtkCustomIdParameterParameter} */
               cp;
+
             for (; i < pLen; ++i) {
               p = parameters[i];
               m = p.name.match(inlineRegex);
+
               if (m && m.length > 1) {
                 p.name = self.get(m[1]);
               }
+
               switch (p.type) {
                 case 'String':
                 case 'MultiLineString':
                   m = p.defaultValue.match(inlineRegex);
+
                   if (m && m.length > 1) {
                     p.defaultValue = self.get(m[1]);
                   }
+
                   break;
+
                 case 'CustomId':
                   cpLen = p.customParam.length;
+
                   for (; j < cpLen; ++j) {
                     cp = p.customParam[j];
                     m = cp.name.match(inlineRegex);
+
                     if (m && m.length > 1) {
                       cp.name = self.get(m[1]);
                     }
                   }
+
                   break;
+
                 default:
                   break;
               }
             }
+
             return parameters;
           }
         };
+
+      // Localization manager is ready!
       return self;
     })(),
     /**
+     * Plugin parameter IDs. So we don't use 'magic values' throughout the code.
+     *
      * @const
+     * @private
      */
     parameterId = {
+      /**
+       * Global variable name plugin parameter ID.
+       *
+       * @const
+       */
       globalVariableName: 0,
+
+      /**
+       * Global variable value plugin parameter ID.
+       *
+       * @const
+       */
       globalVariableValue: 1
     },
     /**
+     * Plugin parameters.
+     *
+     * @const
+     * @private
      * @type {import("pgmmv/agtk/plugins/plugin/parameter").AgtkParameter[]}
      */
     parameters = [
+      // Global variable name parameter.
       {
         id: parameterId.globalVariableName,
         name: 'loca(PARAMETER_NAME_GLOBAL_VARIABLE_NAME)',
         type: 'String',
         defaultValue: 'loca(PARAMETER_DEFAULT_VALUE_GLOBAL_VARIABLE_NAME)'
       },
+      // Global variable value parameter.
       {
         id: parameterId.globalVariableValue,
         name: 'loca(PARAMETER_NAME_GLOBAL_VARIABLE_VALUE)',
@@ -119,11 +225,17 @@
       }
     ],
     /**
+     * Assigned our localized plugin parameters.
+     *
+     * @private
      * @type {import("pgmmv/agtk/plugins/plugin/parameter").AgtkParameter[]}
      */
     localizedParameters,
     /**
+     * Global variable name MUST NOT be one of these.
+     *
      * @const
+     * @private
      */
     reservedWords = [
       'break',
@@ -177,46 +289,62 @@
       'window'
     ],
     /**
-     * @returns {boolean}
+     * Test if plugin is currently running in the editor.
+     *
+     * @returns {boolean} `true` when in editor, `false` otherwise.
+     * @private
      */
     inEditor = function () {
       return !Agtk || typeof Agtk.log !== 'function';
     },
     /**
-     * @returns {Record<number,import("type-fest").JsonValue>}
+     * Normalize specified parameter values into a `<parameter ID>,<value>`
+     * mapping. Missing parameter values will be assigned corresponding values
+     * from the provided defaults, if available.
+     *
+     * @param paramValue Parameter values to normalize.
+     * @param defaults Default parameter values available.
+     * @returns Normalized `<parameter ID>,<value>` mapping.
+     * @private
      */
     normalizeParameters = function (
-      /**
-       * @type {import("pgmmv/agtk/plugins/plugin").AgtkParameterValue[]}
-       */
+      /** @type {import("pgmmv/agtk/plugins/plugin").AgtkParameterValue[]} */
       paramValue,
-      /**
-       * @type {import("pgmmv/agtk/plugins/plugin").AgtkParameterValue[]}
-       */
+      /** @type {import("pgmmv/agtk/plugins/plugin").AgtkParameterValue[]} */
       defaults
     ) {
+      /** @type {Record<number,import("type-fest").JsonValue>} */
       var normalized = {},
         len = defaults.length,
         i = 0,
+        /** @type {import("pgmmv/agtk/plugins/plugin").AgtkParameterValue} */
         p;
+
       for (; i < len; ++i) {
         p = defaults[i];
         normalized[p.id] = p.type === 'Json' ? JSON.stringify(p.defaultValue) : p.defaultValue;
       }
+
       len = paramValue.length;
+
       for (i = 0; i < len; ++i) {
         p = paramValue[i];
         normalized[p.id] = p.value;
       }
+
       return normalized;
     },
     /**
+     * Global variable plugin API.
+     *
      * @type {import("pgmmv/agtk/plugins/plugin").AgtkPlugin}
+     * @public
      */
     self = {
       setLocale: function (locale) {
         locaManager.setLocale(locale);
       },
+
       getInfo: function (category) {
         switch (category) {
           case 'name':
@@ -230,7 +358,7 @@
           case 'parameter':
             return localizedParameters
               ? localizedParameters
-              : (localizedParameters = locaManager.processParameterLocale(parameters));
+              : (localizedParameters = locaManager.processParameters(parameters));
           case 'internal':
             return {};
           case 'actionCommand':
@@ -242,41 +370,53 @@
             break;
         }
       },
+
       initialize: function () {},
+
       finalize: function () {},
+
       setParamValue: function (paramValue) {
         var isError = false,
-          normalizedParameters,
-          globalVariableName,
-          globalVariableValue;
+          /** @type {Record<number,import("type-fest").JsonValue>} */
+          np,
+          /** @type {string} */
+          name,
+          /** @type {import("type-fest").JsonValue} */
+          value;
+
         if (inEditor()) {
           return;
         }
-        normalizedParameters = normalizeParameters(paramValue, self.getInfo('parameter'));
-        globalVariableName = trim(normalizedParameters[parameterId.globalVariableName]);
-        globalVariableValue = JSON.parse(normalizedParameters[parameterId.globalVariableValue]);
-        if (!globalVariableName) {
+
+        np = normalizeParameters(paramValue, self.getInfo('parameter'));
+        name = trim(np[parameterId.globalVariableName]);
+        value = JSON.parse(np[parameterId.globalVariableValue]);
+
+        if (!name) {
           Agtk.log('[Global Variable Plugin] error: global variable name is empty');
           isError = true;
-        } else if (~reservedWords.indexOf(globalVariableName)) {
-          Agtk.log(
-            "[Global Variable Plugin] error: global variable name is a reserved word: '" + globalVariableName + "'"
-          );
+        } else if (~reservedWords.indexOf(name)) {
+          Agtk.log("[Global Variable Plugin] error: global variable name is a reserved word: '" + name + "'");
           isError = true;
-        } else if (/\s/g.test(globalVariableName)) {
-          Agtk.log(
-            "[Global Variable Plugin] error: global variable name contains whitespace: '" + globalVariableName + "'"
-          );
+        } else if (/\s/g.test(name)) {
+          Agtk.log("[Global Variable Plugin] error: global variable name contains whitespace: '" + name + "'");
           isError = true;
         }
+
         if (isError) {
           Agtk.log('[Global Variable Plugin] error: skipping global variable injection');
           return;
         }
-        window[globalVariableName] = globalVariableValue;
+
+        // Inject value into global variable!
+        window[name] = value;
       },
+
       setInternal: function () {},
+
       call: function call() {}
     };
+
+  // Plugin is ready!
   return self;
 })();
